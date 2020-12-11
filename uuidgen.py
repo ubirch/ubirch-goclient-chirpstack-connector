eecdf1d1abbc3dc7aab2a170207ada7ab31af1d3
OK <ole.karge@ubirch.com>
2020-12-11 16:40:12 +0100

    update the uuid generator

--- uuidgen.py
+++ uuidgen.py
@@ -1,10 +1,12 @@
 import uuid
 
-NAMESPACE_NAME = "eon-community"
-NAMESPACE_UUID = uuid.uuid5(uuid.UUID("00000000-0000-0000-0000-000000000000"), NAMESPACE_NAME)
+EON_NAMESPACE_NAME = "eon.uuid.trustservice"
+EON_NAMESPACE_UUID = uuid.uuid5(uuid.UUID("00000000-0000-0000-0000-000000000000"), EON_NAMESPACE_NAME)
+UBIRCH_NAMESPACE_NAME = "ubirch"
+UBIRCH_NAMESPACE_UUID = uuid.uuid5(EON_NAMESPACE_UUID, UBIRCH_NAMESPACE_NAME)
 
 print("|===== UUID Generator =====|")
-print("Namespace: %s/%s" % (NAMESPACE_NAME, str(NAMESPACE_UUID)))
+print("Namespace: %s -> %s / %s -> %s" % (EON_NAMESPACE_NAME, UBIRCH_NAMESPACE_NAME, str(EON_NAMESPACE_UUID), str(UBIRCH_NAMESPACE_UUID)))
 devEUI = input("DevEUI > ")
-uuid = uuid.uuid5(NAMESPACE_UUID, devEUI)
+uuid = uuid.uuid5(UBIRCH_NAMESPACE_UUID, devEUI)
 print("UUID: %s" % str(uuid))
