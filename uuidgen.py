2f8ad3df0d471f0c9b7fe6920a97c3a3e957b22a
OK <ttnuser@protonmail.com>
2020-12-19 18:02:19 +0100

    make uuidgen.py useable as module

--- uuidgen.py
+++ uuidgen.py
@@ -5,8 +5,16 @@ EON_NAMESPACE_UUID = uuid.uuid5(uuid.UUID("00000000-0000-0000-0000-000000000000"
 UBIRCH_NAMESPACE_NAME = "ubirch"
 UBIRCH_NAMESPACE_UUID = uuid.uuid5(EON_NAMESPACE_UUID, UBIRCH_NAMESPACE_NAME)
 
-print("|===== UUID Generator =====|")
-print("Namespace: %s -> %s / %s -> %s" % (EON_NAMESPACE_NAME, UBIRCH_NAMESPACE_NAME, str(EON_NAMESPACE_UUID), str(UBIRCH_NAMESPACE_UUID)))
-devEUI = input("DevEUI > ")
-uuid = uuid.uuid5(UBIRCH_NAMESPACE_UUID, devEUI)
-print("UUID: %s" % str(uuid))
+
+def getDevUUID(devEUI : str) -> uuid.UUID:
+  return uuid.uuid5(UBIRCH_NAMESPACE_UUID, devEUI)
+
+def main():
+  print("|===== UUID Generator =====|")
+  print("Namespace: %s -> %s / %s -> %s" % (EON_NAMESPACE_NAME, UBIRCH_NAMESPACE_NAME, str(EON_NAMESPACE_UUID), str(UBIRCH_NAMESPACE_UUID)))
+  devEUI = input("DevEUI > ")
+  print("UUID: %s" % str(getDevUUID(devEUI)))
+
+
+if __name__ == "__main__":
+  main()
