9641d8089b744b85034c16d14ba949ac5b8a164d
OK <ttnuser@protonmail.com>
2021-01-26 21:39:33 +0100

    move uuidgen to src/

--- uuidgen.py
+++ uuidgen.py
@@ -1,19 +1,11 @@
 import uuid
-
-EON_NAMESPACE_NAME = "eon.uuid.trustservice"
-EON_NAMESPACE_UUID = uuid.uuid5(uuid.UUID("00000000-0000-0000-0000-000000000000"), EON_NAMESPACE_NAME)
-UBIRCH_NAMESPACE_NAME = "ubirch"
-UBIRCH_NAMESPACE_UUID = uuid.uuid5(EON_NAMESPACE_UUID, UBIRCH_NAMESPACE_NAME)
-
-
-def getDevUUID(devEUI : str) -> uuid.UUID:
-  return uuid.uuid5(UBIRCH_NAMESPACE_UUID, devEUI)
+import src.uuidgen
 
 def main():
   print("|===== UUID Generator =====|")
-  print("Namespace: %s -> %s / %s -> %s" % (EON_NAMESPACE_NAME, UBIRCH_NAMESPACE_NAME, str(EON_NAMESPACE_UUID), str(UBIRCH_NAMESPACE_UUID)))
+  print("Namespace: %s -> %s / %s -> %s" % (src.uuidgen.EON_NAMESPACE_NAME, src.uuidgen.UBIRCH_NAMESPACE_NAME, str(src.uuidgen.EON_NAMESPACE_UUID), str(src.uuidgen.UBIRCH_NAMESPACE_UUID)))
   devEUI = input("DevEUI > ")
-  print("UUID: %s" % str(getDevUUID(devEUI)))
+  print("UUID: %s" % str(src.uuidgen.getDevUUID(devEUI)))
 
 
 if __name__ == "__main__":
